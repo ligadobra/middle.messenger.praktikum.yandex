930e44a9f3eea95c28718226a75d4654c47f7dcd
ligadobra <russiancmo@gmail.com>
2022-06-19 01:51:14 +0300

    add dos and xss security

--- index.ts
+++ index.ts
@@ -1,19 +1,42 @@
 import express from "express";
+import rateLimit from "express-rate-limit";
 
 const app = express();
+
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+app.use(function (req, res, next) {
+  res.setHeader("X-XSS-Protection", "1; mode=block");
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self' https://ya-praktikum.tech wss://ya-praktikum.tech; font-src 'self'; img-src 'self' https://ya-praktikum.tech; script-src 'self'; style-src 'self' 'unsafe-inline'; frame-src 'self'"
+  );
+  return next();
+});
 
 app.use(express.static(__dirname + "/dist"));
 app.use(express.static(__dirname + "/public"));
 app.use("/svg", express.static(__dirname + "/src/assets/svg"));
 app.use("/fonts", express.static(__dirname + "/src/assets/fonts"));
 
-app.get("/", (req, res) => res.sendFile(__dirname + "/dist/index.html"));
-app.get("/sign-up", (req, res) =>
+app.get("/", apiLimiter, (req, res) =>
   res.sendFile(__dirname + "/dist/index.html")
 );
-app.get("/messenger", (req, res) => res.sendFile(__dirname + "/dist/index.html"));
-app.get("/settings", (req, res) => res.sendFile(__dirname + "/dist/index.html"));
-app.get("*", (req, res) => {
+app.get("/sign-up", apiLimiter, (req, res) =>
+  res.sendFile(__dirname + "/dist/index.html")
+);
+app.get("/messenger", apiLimiter, (req, res) =>
+  res.sendFile(__dirname + "/dist/index.html")
+);
+app.get("/settings", apiLimiter, (req, res) =>
+  res.sendFile(__dirname + "/dist/index.html")
+);
+app.get("*", apiLimiter, (req, res) => {
   if (res.status(404)) {
     res.sendFile(__dirname + "/public/404.html");
   }
@@ -22,4 +45,6 @@ app.get("*", (req, res) => {
   }
 });
 
-app.listen(process.env.PORT || 3000, () => console.log(`Listening on port ${process.env.PORT || 3000}!`));
+app.listen(process.env.PORT || 3000, () =>
+  console.log(`Listening on port ${process.env.PORT || 3000}!`)
+);
